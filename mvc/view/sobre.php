448edaebb208bb4a82e4ee659cf8b3c84d9082e0
Andrei Freire de Almeida <andreifalmeida08@gmail.com>
2026-04-04 14:16:46 -0400

    Update sobre.php
    corrigindo o problema de diretório com as fotos dos membors do sobre nós

--- mvc/view/sobre.php
+++ mvc/view/sobre.php
@@ -14,31 +14,31 @@
     <h1>sobre nós</h1>
     <div class="membros">
         <div class="bio">
-            <img src="fotos_membro/andreiperfil.jpeg" width="270px" height="270px" alt="perfildoandrei">
+            <img src="fotos_membros/andreiperfil.jpeg" width="270px" height="270px" alt="perfildoandrei">
             <h2>Andrei</h2>
             <p>Desenvolvedor Back-end, responsável pela <br>inteligência e lógica do site.</p>
         </div>
 
         <div class="bio">
-            <img src="fotos_membro/carlosperfil.jpeg" alt="perfildocarlos" width="270px" height="270px">
+            <img src="fotos_membros/carlosperfil.jpeg" alt="perfildocarlos" width="270px" height="270px">
             <h2>Carlos </h2>
             <p>Desenvolvedor Front-end, focado na interface<br> e interação do usuário.</p>
         </div>
 
         <div class="bio">
-            <img src="fotos_membro/mariaperfil.jpeg" alt="perfildamaria" width="270px" height="270px">
+            <img src="fotos_membros/mariaperfil.jpeg" alt="perfildamaria" width="270px" height="270px">
             <h2>Maria Lara</h2>
             <p> Web Designer, cuidando da identidade visual <br>e do layout inicial.</p>
         </div>
 
         <div class="bio">
-            <img src="fotos_membro/sofiperfil.jpeg" alt="perfildasofia" width="270px" height="270px">
+            <img src="fotos_membros/sofiperfil.jpeg" alt="perfildasofia" width="270px" height="270px">
             <h2>Sofia Hage</h2>
             <p> Líder da equipe, responsável pela gestão<br> e organização do projeto.</p>
         </div>
 
         <div class="bio">
-            <img src="fotos_membro/larissaperfil.jpeg" alt="perfilarissa" width="270px" height="270px">
+            <img src="fotos_membros/larissaperfil.jpeg" alt="perfilarissa" width="270px" height="270px">
             <h2>Larissa</h2>
             <p>Quality Assurance (QA)<br> e Banco de Dados, responsável<br> pelos testes e pela segurança das
                 informações.</p>
