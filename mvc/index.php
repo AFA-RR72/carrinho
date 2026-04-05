c3fb94e5a65849cc9ecae055b818953d38662b89
Andrei Freire de Almeida <andreifalmeida08@gmail.com>
2026-04-04 22:47:40 -0400

    atualição css

--- mvc/index.php
+++ mvc/index.php
@@ -4,23 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário</title>
-    <link rel="stylesheet" href="view/form.css">
+    <link rel="stylesheet" href="view/index.css">
 </head>
 <body>
     <form action="view/conta.php" method="post"> 
         <div class="container">
             <div class="card">
-        <label for="nome">Nome: <input type="text" name="nome" placeholder="Nome"></label>
+        <label for="nome" class="input">Nome: <input type="text" name="nome" placeholder="Nome"></label>
         <br>
-        <label for="email">E-mail: <input type="email" name="email" placeholder="exemplo@gmail.com"></label>
+        <label for="email" class="input">E-mail: <input type="email" name="email" placeholder="exemplo@gmail.com"></label>
         <br>
-        <label for="senha">Senha:</label>
+        <label for="senha" class="input">Senha:</label>
         <input type="password" placeholder="********" name="senha">
         <button type="submit">Enviar</button>
             </div>
         </div>
-
     </form>
-
 </body>
 </html>
